b3369650212f587370bf6715967e6b1cfb5c4b43
dainakai <dainakai1031@icloud.com>
2021-11-18 18:04:38 +0900

    a

--- forkasika/generalclose.cu
+++ forkasika/generalclose.cu
@@ -1,7 +1,7 @@
 /*****************************************************************************
 * Dai NAKAI
 * Three dimensional Droplets distribution and its hologram generating software
-* 2021/5/13
+* 2021/5/22
 * 
 ******************************************************************************/
 #include <bits/stdc++.h>
@@ -26,17 +26,19 @@ using namespace std;
     }                                                                           \
 }
 
-//Confirm ref image when you change width or height
-const char* ref_img_path = "./1024.bmp";
-const char* output_image_name = "output_holo.bmp";
-const char* output_dir = "./holograms";
+//Confirm ref image when you change width or height. 
+//ref-img should be half of output.
+const char* ref_img_path = "../1024.bmp";
+// const char* output_dir = "../holograms/train/close_holo";
+char output_dir_num[100];
 unsigned char header_buf[1078];
 char output_path[100];
 
-const int height = 1024;
-const int width = 1024;
-const int depth = 2048;
-const int particle_num = 26;
+const int height = 2048;
+const int width = 2048;
+const int depth = 1024;
+// const int particle_num = 17;
+// const int image_count = 3000;
 
 //All units provided by micro meter
 const float mean_diam = 50.0;
@@ -46,9 +48,9 @@ const float dist_to_cam = 50000.0;
 const float peak_bright = 127;
 
 void particle_diam_dist (float *diam, int index);
-void particle_posi_dist (float *x, float *y, float*z);
+void particle_posi_dist (float *x, float *y, float*z, int particle_num);
 void z_axis_sort (float *array, int left, int right);
-void close_plane_info (float *info, float *close_info);
+void close_plane_info (float *info, float *close_info, int particle_num);
 
 __global__ void initialize_holo_plane (cufftComplex *holo);
 __global__ void particle_volume (float *info, unsigned char *out, float *close_info);
@@ -63,89 +65,9 @@ FILE *fp;
 /**********************************main***********************************/
 int main(int argc, char** argv){
     printf("%s Starting...\n", argv[0]);
-
-    float host_diam[particle_num];
-    float host_posi_x[particle_num], host_posi_y[particle_num], host_posi_z[particle_num];
-    unsigned char *dev_V;
-    float *host_particle_info, *dev_particle_info;
-    float *dev_float_image, float_image[height][width];
-    unsigned char image_out[height][width];
-    float host_close_info[7], *dev_close_info;
-
-    int dev = 0;
-    cudaSetDevice(dev);
-
-    mkdir(output_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
-    srand((unsigned int)time(NULL));
-
-    particle_diam_dist(host_diam, particle_num);
-    particle_posi_dist(host_posi_x, host_posi_y, host_posi_z);
-    z_axis_sort(host_posi_z, 0, particle_num-1);
-
-    host_particle_info = (float *)malloc(sizeof(float)*particle_num*4);
-    for (int i = 0; i < particle_num; i++){
-        host_particle_info[4*i] = host_diam[i];
-        host_particle_info[4*i+1] = host_posi_x[i];
-        host_particle_info[4*i+2] = host_posi_y[i];
-        host_particle_info[4*i+3] = host_posi_z[i];
-        printf("%lf %lf %lf %lf\n",host_diam[i],host_posi_x[i],host_posi_y[i],host_posi_z[i]);
-    }
-
-    close_plane_info(host_particle_info, host_close_info);
-
-    CHECK(cudaMalloc((void **)&dev_particle_info, sizeof(float)*particle_num*4));
-    CHECK(cudaMalloc((void **)&dev_V, sizeof(unsigned char)*width*height*particle_num));
-    CHECK(cudaMalloc((void **)&dev_close_info, sizeof(float)*7));
-
-    CHECK(cudaMemcpy(dev_particle_info, host_particle_info, sizeof(float)*particle_num*4, cudaMemcpyHostToDevice));
-    CHECK(cudaMemcpy(dev_close_info, host_close_info, sizeof(float)*7, cudaMemcpyHostToDevice));
-
-    dim3 grid(particle_num, width, height), block(1);
-    dim3 grid2(width,height), block2(1);
-
-    cufftHandle plan;
-    cufftPlan2d(&plan,width,height,CUFFT_C2C);
-    cufftComplex *devc_object, *devc_hologram, *devc_trans;
-    CHECK(cudaMalloc((void **)&devc_object, sizeof(cufftComplex)*width*height)); 
-    CHECK(cudaMalloc((void **)&devc_hologram, sizeof(cufftComplex)*width*height)); 
-    CHECK(cudaMalloc((void **)&devc_trans, sizeof(cufftComplex)*width*height)); 
-
-
-    particle_volume<<<grid, block>>>(dev_particle_info, dev_V, dev_close_info);
-
-    float dist_to_next_holo;
-
-    extract_plane_from_vol<<<grid2, block2>>>(dev_V, devc_object, 0);
-    for (int itr = 0; itr < particle_num - 1; itr++) {
-        cufftExecC2C(plan, devc_object, devc_object, CUFFT_FORWARD);
-        fftshift_2D<<<grid2, block2>>>(devc_object);
-        dist_to_next_holo = host_particle_info[4*(itr+1) + 3] - host_particle_info[4*itr + 3];
-        trans_func<<<grid2, block2>>>(devc_trans, dist_to_next_holo);
-        plane_complex_multiple<<<grid2, block2>>>(devc_object, devc_trans, devc_hologram);
-        fftshift_2D<<<grid2, block2>>>(devc_hologram);
-        cufftExecC2C(plan, devc_hologram, devc_hologram, CUFFT_INVERSE);
-        two_dim_divide_for_fft<<<grid2,block2>>>(devc_hologram);
-        extract_plane_from_vol<<<grid2, block2>>>(dev_V, devc_object, itr+1);
-        plane_complex_multiple<<<grid2, block2>>>(devc_object, devc_hologram, devc_object);
-    }
-    cufftExecC2C(plan, devc_object, devc_object, CUFFT_FORWARD);
-    fftshift_2D<<<grid2, block2>>>(devc_object);
-    trans_func<<<grid2, block2>>>(devc_trans,  dist_to_cam + (float)depth*DX - host_particle_info[4*(particle_num-1) + 3]);
-    plane_complex_multiple<<<grid2, block2>>>(devc_object, devc_trans, devc_hologram);
-    fftshift_2D<<<grid2, block2>>>(devc_hologram);
-    cufftExecC2C(plan, devc_hologram, devc_hologram, CUFFT_INVERSE);
-    two_dim_divide_for_fft<<<grid2,block2>>>(devc_hologram);
-
-    CHECK(cudaMalloc((void **)&dev_float_image, sizeof(float)*width*height));
-    holo_to_float_image<<<grid2, block2>>>(devc_hologram, dev_float_image);
-
-    CHECK(cudaMemcpy(float_image, dev_float_image, sizeof(float)*width*height, cudaMemcpyDeviceToHost));
-
-    for (int y=0; y < height; y++) {
-        for (int x=0; x < width; x++) {
-            image_out[y][x] = (unsigned char)(peak_bright*float_image[y][x]);
-        }
-    }
+    char *output_dir = argv[1];
+    int particle_num = atoi(argv[2]);
+    int image_count = atoi(argv[3]);
 
     fp = fopen(ref_img_path,"rb");
     if(fp == NULL){
@@ -156,11 +78,101 @@ int main(int argc, char** argv){
     read_conf = fread(header_buf, sizeof(unsigned char), 1078, fp);
     fclose(fp);
 
-    sprintf(output_path, "%s/%s",output_dir,output_image_name);
-    fp = fopen(output_path, "wb");
-    fwrite(header_buf, sizeof(unsigned char), 1078, fp);
-    fwrite(image_out, sizeof(unsigned char), width*height, fp);
-    fclose(fp);
+    float host_diam[particle_num];
+    float host_posi_x[particle_num], host_posi_y[particle_num], host_posi_z[particle_num];
+    unsigned char *dev_V;
+    float *host_particle_info, *dev_particle_info;
+    float *dev_float_image, float_image[height/2][width/2];
+    unsigned char image_out[height/2][width/2];
+    float host_close_info[7], *dev_close_info;
+    float dist_to_next_holo;
+
+    int dev = 0;
+    cudaSetDevice(dev);
+
+    dim3 grid(particle_num, width, height), block(1);
+    dim3 grid2(width,height), block2(1);
+    dim3 grid3(width/2,height/2), block3(1);
+
+    CHECK(cudaMalloc((void **)&dev_particle_info, sizeof(float)*particle_num*4));
+    CHECK(cudaMalloc((void **)&dev_V, sizeof(unsigned char)*width*height*particle_num));
+    CHECK(cudaMalloc((void **)&dev_close_info, sizeof(float)*7));
+    CHECK(cudaMalloc((void **)&dev_float_image, sizeof(float)*width*height/4));
+
+    cufftHandle plan;
+    cufftPlan2d(&plan,width,height,CUFFT_C2C);
+    cufftComplex *devc_object, *devc_hologram, *devc_trans;
+    CHECK(cudaMalloc((void **)&devc_object, sizeof(cufftComplex)*width*height)); 
+    CHECK(cudaMalloc((void **)&devc_hologram, sizeof(cufftComplex)*width*height)); 
+    CHECK(cudaMalloc((void **)&devc_trans, sizeof(cufftComplex)*width*height));
+
+    sprintf(output_dir_num,"%s/num_%05d/",output_dir,particle_num);
+    printf("%s\n",output_dir_num);
+    mkdir(output_dir_num, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    srand((unsigned int)time(NULL));
+
+    for(int count = 0; count < image_count; count++){
+        particle_diam_dist(host_diam, particle_num);
+        particle_posi_dist(host_posi_x, host_posi_y, host_posi_z, particle_num);
+        z_axis_sort(host_posi_z, 0, particle_num-1);
+
+        host_particle_info = (float *)malloc(sizeof(float)*particle_num*4);
+        for (int i = 0; i < particle_num; i++){
+            host_particle_info[4*i] = host_diam[i];
+            host_particle_info[4*i+1] = host_posi_x[i];
+            host_particle_info[4*i+2] = host_posi_y[i];
+            host_particle_info[4*i+3] = host_posi_z[i];
+            printf("%lf %lf %lf %lf\n",host_diam[i],host_posi_x[i],host_posi_y[i],host_posi_z[i]);
+        }
+
+        close_plane_info(host_particle_info, host_close_info, particle_num);
+
+        CHECK(cudaMemcpy(dev_particle_info, host_particle_info, sizeof(float)*particle_num*4, cudaMemcpyHostToDevice));
+        CHECK(cudaMemcpy(dev_close_info, host_close_info, sizeof(float)*7, cudaMemcpyHostToDevice));
+
+        particle_volume<<<grid, block>>>(dev_particle_info, dev_V, dev_close_info);
+
+        extract_plane_from_vol<<<grid2, block2>>>(dev_V, devc_object, 0);
+        for (int itr = 0; itr < particle_num - 1; itr++) {
+            cufftExecC2C(plan, devc_object, devc_object, CUFFT_FORWARD);
+            fftshift_2D<<<grid2, block2>>>(devc_object);
+            dist_to_next_holo = host_particle_info[4*(itr+1) + 3] - host_particle_info[4*itr + 3];
+            trans_func<<<grid2, block2>>>(devc_trans, dist_to_next_holo);
+            plane_complex_multiple<<<grid2, block2>>>(devc_object, devc_trans, devc_hologram);
+            fftshift_2D<<<grid2, block2>>>(devc_hologram);
+            cufftExecC2C(plan, devc_hologram, devc_hologram, CUFFT_INVERSE);
+            two_dim_divide_for_fft<<<grid2,block2>>>(devc_hologram);
+            extract_plane_from_vol<<<grid2, block2>>>(dev_V, devc_object, itr+1);
+            plane_complex_multiple<<<grid2, block2>>>(devc_object, devc_hologram, devc_object);
+        }
+        cufftExecC2C(plan, devc_object, devc_object, CUFFT_FORWARD);
+        fftshift_2D<<<grid2, block2>>>(devc_object);
+        trans_func<<<grid2, block2>>>(devc_trans,  dist_to_cam + (float)depth*DX - host_particle_info[4*(particle_num-1) + 3]);
+        plane_complex_multiple<<<grid2, block2>>>(devc_object, devc_trans, devc_hologram);
+        fftshift_2D<<<grid2, block2>>>(devc_hologram);
+        cufftExecC2C(plan, devc_hologram, devc_hologram, CUFFT_INVERSE);
+        two_dim_divide_for_fft<<<grid2,block2>>>(devc_hologram);
+
+        holo_to_float_image<<<grid3, block3>>>(devc_hologram, dev_float_image);
+
+        CHECK(cudaMemcpy(float_image, dev_float_image, sizeof(float)*width*height/4, cudaMemcpyDeviceToHost));
+
+        for (int y=0; y < height/2; y++) {
+            for (int x=0; x < width/2; x++) {
+                image_out[y][x] = (unsigned char)(peak_bright*float_image[y][x]);
+            }
+        }
+
+        sprintf(output_path, "%s/%05d.bmp",output_dir_num,count);
+        fp = fopen(output_path, "wb");
+        fwrite(header_buf, sizeof(unsigned char), 1078, fp);
+        fwrite(image_out, sizeof(unsigned char), width*height/4, fp);
+        fclose(fp);
+
+        printf("%d of %d has processed\n",count+1,image_count);
+        printf("\n\n");
+
+    }
 
     free(host_particle_info);
     cudaFree(dev_particle_info);
@@ -176,10 +188,10 @@ int main(int argc, char** argv){
     return 0;
 }
 
-void particle_posi_dist (float *x, float *y, float*z){
+void particle_posi_dist (float *x, float *y, float*z, int particle_num){
     for (int i = 0; i < particle_num; i++) {
-        x[i] = rand() % width * DX;
-        y[i] = rand() % height * DX;
+        x[i] = (rand() % (width/2) + width/4.0) * DX;
+        y[i] = (rand() % (height/2) + height/4.0) * DX;
         z[i] = rand() % depth * DX;
     }
 }
@@ -226,9 +238,9 @@ void z_axis_sort (float *array, int left, int right){
             z_axis_sort(array,j+1,right);
 }
 
-void close_plane_info (float *info, float *close_info){
-    close_info[0] = ((float)(rand() % (int)(width/2)) + (float)width/4)*DX;
-    close_info[1] = ((float)(rand() % (int)(height/2)) + (float)height/4)*DX;
+void close_plane_info (float *info, float *close_info, int particle_num){
+    close_info[0] = (rand() % (width/4) + 3.0*width/8.0) * DX;
+    close_info[1] = (rand() % (height/4) + 3.0*height/8.0) * DX;
     
     float diam[2];
     particle_diam_dist(diam, 2);
@@ -355,7 +367,7 @@ __global__ void holo_to_float_image (cufftComplex *data, float *image){
 	int x = blockIdx.x;
     int y = blockIdx.y;
 
-    image[x + width*y] = sqrt(data[x + width*y].x*data[x + width*y].x + data[x + width*y].y*data[x + width*y].y);
+    image[x + width/2*y] = sqrt(data[(x + width/4) + width*(y + height/4)].x*data[(x + width/4) + width*(y + height/4)].x + data[(x + width/4) + width*(y + height/4)].y*data[(x + width/4) + width*(y + height/4)].y);
 
 }
 
